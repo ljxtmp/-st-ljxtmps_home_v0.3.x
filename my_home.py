96b48f1f87b10eaa3abbc21493914e2f355519ef
ljxtmp <ljx_XueXi_temp_014@hotmail.com>
2024-07-29 14:22:41 +0800

    my_home.py

--- my_home.py
+++ my_home.py
@@ -338,11 +338,5 @@ def main():
     st.write('BGM:霞光')
     st.audio(bgm, format='audio/mp3', start_time=0)
 
-def fixing():
-    page = st.sidebar.radio('我的主页', ['首页', '我的兴趣推荐', '计算器', '图片处理工具', '智慧词典', '网址导航', 'RGB调色板', '留言区'])
-    st.write(page)
-    st.write('')
-    st.write('网站维护中…')
-
 
 main()
